560215bc5fb4a5a233be52929a39769c10f41fff
Nathan <test@gmail.com>
2024-10-14 14:59:43 -0400

    hard paths

--- experiment_1_main/run.sh
+++ experiment_1_main/run.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname $SCRIPT)
 TIMEOUT=$(( 24 * 3600 ))
 
 timestamp=$(date '+%y-%m-%d-%H-%M')
-revizor_src='./revizor'
+revizor_src='/home/njiang/revizor-artifact/revizor'
 instructions="$revizor_src/src/x86/executor/base.json"
 
 exp_dir="/home/njiang/revizor-artifact/results/experiment_1/$timestamp"
