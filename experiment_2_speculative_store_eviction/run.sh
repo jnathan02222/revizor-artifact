94cb935fdae7af4d25f783af59f52016531db42a
jnathan0222 <123518866+jnathan0222@users.noreply.github.com>
2024-10-16 12:17:06 -0400

    run.sh change

--- experiment_2_speculative_store_eviction/run.sh
+++ experiment_2_speculative_store_eviction/run.sh
@@ -6,13 +6,14 @@ SCRIPT_DIR=$(dirname $SCRIPT)
 TIMEOUT=$(( 1 * 3600 ))
 
 timestamp=$(date '+%y-%m-%d-%H-%M')
-revizor_src='revizor/src'
-instructions="$revizor_src/instruction_sets/x86/base.xml"
+revizor_src='/home/njiang/revizor-artifact/revizor'
+instructions="$revizor_src/src/x86/executor/base.json"
 
-exp_dir="results/experiment_2/$timestamp"
+exp_dir="/home/njiang/revizor-artifact/results/experiment_2/$timestamp"
 mkdir $exp_dir
 
 log="$exp_dir/experiment.log"
 touch $log
 
-${revizor_src}/cli.py fuzz -s $instructions -n 10000 -i 50 --timeout $TIMEOUT -v -w $exp_dir -c $SCRIPT_DIR/full-ct-nonspec-cond.yaml 2>&1 | tee -a $log
+cd ${revizor_src}
+rvzr fuzz -s $instructions -n 10000 -i 50 --timeout $TIMEOUT -w $exp_dir -c $SCRIPT_DIR/full-ct-nonspec-cond.yaml 2>&1 | tee -a $log
