bd49426dc84af3878924b6e39a7a86c403d6f129
jnathan0222 <123518866+jnathan0222@users.noreply.github.com>
2024-10-16 13:31:40 -0400

    Experiment 4

--- experiment_4_arch_vs_ct/run.sh
+++ experiment_4_arch_vs_ct/run.sh
@@ -5,10 +5,10 @@ SCRIPT=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT)
 
 timestamp=$(date '+%y-%m-%d-%H-%M')
-revizor_src='./revizor/src'
-instructions="$revizor_src/instruction_sets/x86/base.xml"
+revizor_src='/home/njiang/revizor-artifact/revizor'
+instructions="$revizor_src/src/x86/executor/base.json"
 
-exp_dir="results/experiment_4/$timestamp"
+exp_dir="/home/njiang/revizor-artifact/results/experiment_4/$timestamp"
 mkdir $exp_dir
 
 log="$exp_dir/experiment.log"
@@ -16,10 +16,12 @@ touch $log
 
 # Violation of CT-SEQ
 echo "------------------ Testing against CT-SEQ ---------------------------"
-${revizor_src}/cli.py fuzz -s $instructions -n 10000 -i 100 -v -w $exp_dir -c $SCRIPT_DIR/v1-ct-seq.yaml 2>&1 | tee -a $log
+cd ${revizor_src}
+rvzr fuzz -s $instructions -n 10000 -i 100 -w $exp_dir -c $SCRIPT_DIR/v1-ct-seq.yaml 2>&1 | tee -a $log
 mv $exp_dir/violation*.asm $exp_dir/ct-seq-violation.asm
 
 # Violation of ARCH-SEQ
 echo "------------------ Testing against ARCH-SEQ ---------------------------"
-${revizor_src}/cli.py fuzz -s $instructions -n 10000 -i 100 -v -w $exp_dir -c $SCRIPT_DIR/v1-arch-seq.yaml 2>&1 | tee -a $log
+cd ${revizor_src}
+rvzr fuzz -s $instructions -n 10000 -i 100 -w $exp_dir -c $SCRIPT_DIR/v1-arch-seq.yaml 2>&1 | tee -a $log
 mv $exp_dir/violation*.asm $exp_dir/arch-seq-violation.asm
